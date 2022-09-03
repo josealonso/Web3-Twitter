c3ae5a2f425958af47bec490e025f1b39b5ead96
JR <jose.alonso.programmer@gmail.com>
2022-09-03 16:33:18 +0200

    Create the sidebars

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -5,6 +5,7 @@ import netflix from "../images/netflix.jpeg";
 import academy from "../images/academy.png";
 import youtube from "../images/youtube.png";
 import js from "../images/js.png";
+import { Input } from "web3uikit";
 
 
 const Rightbar = () => {
@@ -38,6 +39,31 @@ const Rightbar = () => {
 
   return (
     <>
+      <div className="rightbarContent">
+        <Input
+          label="Search Twitter"
+          name="Search Twitter"
+          prefixIcon="search"
+          labelBgColor="#141d26"
+        >
+        </Input>
+        <div className="trends">
+          News for you
+          {trends.map((e) => {
+            return (
+              <>
+                <div className="trend" onClick={() => window.open(e.link)} >
+                  <img src={e.img} className="trendImg"></img>
+                  <div className="trendText">{e.text}</div>
+                </div>
+              </>
+            )
+          })}
+
+        </div>
+
+      </div>
+
     </>
   );
 };
